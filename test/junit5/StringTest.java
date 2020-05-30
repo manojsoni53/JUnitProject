c42ac538a1a6d4bbb7a5a18913ea506b0cdee6f6
manojsoni53 <manojsoni2091@gmail.com>
2020-05-30 20:32:03 +0530

    @BeforeAll and @AfterAll

--- test/junit5/StringTest.java
+++ test/junit5/StringTest.java
@@ -2,14 +2,26 @@ package junit5;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
 class StringTest {
 
-	@BeforeEach
+	@BeforeAll			  //Initialize once before any of the method or all method, even before @BeforeEach
+	static void beforeAll() {
+		System.out.println("Initialize connection");
+	}
+	
+	@AfterAll
+	static void afterAll() {
+		System.out.println("Clossing connection");
+	}
+	
+	@BeforeEach		//Initialize before every method in class event run for single method
 	void beforeEach(TestInfo info) {		//parameter is optional, in junit there was no parameter
 		System.out.println("Initialize test data for "+info.getDisplayName());
 	}
