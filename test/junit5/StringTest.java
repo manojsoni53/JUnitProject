60f2fc2508344e744efc892d4934b2c06e42457e
manojsoni53 <manojsoni2091@gmail.com>
2020-05-30 18:34:03 +0530

    more on assert

--- test/junit5/StringTest.java
+++ test/junit5/StringTest.java
@@ -12,7 +12,26 @@ class StringTest {
 		int actualLength = "ABCD".length();
 		int expectedLength = 4;
 		
-		assertEquals(expectedLength, actualLength);
+		assertEquals(expectedLength, actualLength);	//int parameter
+	}
+	
+	@Test
+	void toUpperCase_basic() {
+		 String str = "abcd";
+		 String result = str.toUpperCase();
+		 assertNotNull(result);
+		 //duplicate test
+		 assertEquals("ABCD", result);			//String parameter		
+	}
+	
+	@Test
+	void contains_basic() {
+		String str="abcdefgh";
+		boolean result = str.contains("ijk");
+		
+		//assertEquals(false, result);
+		//assertFalse(result);
+		assertTrue(!result);
 	}
 
 }
